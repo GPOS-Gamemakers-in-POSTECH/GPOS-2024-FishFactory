ff91851af0b2acb401937d2adf04b30823394018
2nter21 <155376343+2nter21@users.noreply.github.com>
2024-08-12 01:28:50 +0900

    Fade in/out
    씬 전환 시에 페이드 인/아웃 기능 구현

--- Assets/AnJY/Scripts/ArrowInteraction.cs
+++ Assets/AnJY/Scripts/ArrowInteraction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ArrowInteraction : MonoBehaviour
 {
@@ -12,10 +13,18 @@ public class ArrowInteraction : MonoBehaviour
     // save the name of departureMap to choose the correct position of player
     public static string departureMap;
 
-
     [SerializeField] 
-    private string sceneName;
+    private string sceneName; // select scene to move into
+
+    public Image fadeImage;
+    private float fadeDuration = 1f;
     
+    void Start()
+    {
+        // fade in
+        StartCoroutine(FadeFunction(1f));
+    }
+
     void Update()
     {
         // calculate distance between player and arrow
@@ -27,10 +36,36 @@ public class ArrowInteraction : MonoBehaviour
             //Debug.Log("Close Enough to Interact");
             if (Input.GetKeyDown(interactionKey))
             {
-                departureMap = SceneManager.GetActiveScene().name;
-                Debug.Log("departure map is : " + departureMap);
-                SceneManager.LoadScene(sceneName);
+                StartCoroutine(FadeAndLoadScene());
             }
+        }
+    }
+
+    IEnumerator FadeAndLoadScene()
+    {
+        // fade out
+        yield return StartCoroutine(FadeFunction(0f));
+
+        // save the departureMap name
+        departureMap = SceneManager.GetActiveScene().name;
+        Debug.Log("departure map is : " + departureMap);
+        
+        // load the new scene
+        SceneManager.LoadScene(sceneName);
+
+
+    }
+
+    IEnumerator FadeFunction(float index) // 0 : fade out, 1 : fade in
+    {
+        float elapsedTIme = 0f;
+        Color color = fadeImage.color;
+        while (elapsedTIme < fadeDuration)
+        {
+            elapsedTIme += Time.deltaTime;
+            color.a = Mathf.Lerp(index, 1f - index, elapsedTIme / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
         }
     }
 }
