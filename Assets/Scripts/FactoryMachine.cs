fcb1d899ee137cf57a078c77b745d0f2f4c07a5a
psljm <88769528+psljm@users.noreply.github.com>
2024-08-11 19:41:53 +0900

    FactoryMachine first finishing ver.
    I will pull request this

--- Assets/Scripts/FactoryMachine.cs
+++ Assets/Scripts/FactoryMachine.cs
@@ -3,48 +3,114 @@ public class FactoryMachine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        willCheckTier(int Tier);
+        willMakeMachine(int Tier);
     }
 
-    func willCheckTier(int Tier)
+    // Make factory machine according to the factory tier
+    func willMakeMachine(int Tier)
     {
         switch (Tier)
         {
-            case 1:
-                NormalSale = 1; // Sanjijiksong
-                break;
             case 2:
-                NormalSale = 1;
-                Freezing = 1; // Naengdong
-                Drying = 1; // Malligi
+                if willMakeFreezer() {
+                    Freezer = 1; // Naengdong-gi
+                }
+                if willMakePackagingMachine() {
+                    PackagingMachine = 1; // Pojang-gi
+                }
+                if willMakeDryer() {
+                    Dryer = 1; // Geonjogi
+                }
                 break;
             case 3:
-                NormalSale = 1;
-                Freezing = 1;
-                Drying = 1;
-                Pickle = 1; // Jutgal
+                if willMakeFreezer() {
+                    Freezer = 1; // Naengdong-gi
+                }
+                if willMakePackagingMachine() {
+                    PackagingMachine = 1; // Pojang-gi
+                }
+                if willMakeDryer() {
+                    Dryer = 1; // Geonjogi
+                }
+                if willMakeBoneBreaker() {
+                    BoneBreaker = 1; // Balgolgi
+                }
+                if willMakeSaltedFishCharger() {
+                    SaltedFishCharger = 1; // JeotgalChungjun-gi
+                }
                 break;
             case 4:
-                NormalSale = 1;
-                Freezing = 1;
-                Drying = 1;
-                Pickle = 1;
-                FishCake = 1; // Eomuk
+                if willMakeFreezer() {
+                    Freezer = 1; // Naengdong-gi
+                }
+                if willMakePackagingMachine() {
+                    PackagingMachine = 1; // Pojang-gi
+                }
+                if willMakeDryer() {
+                    Dryer = 1; // Geonjogi
+                }
+                if willMakeBoneBreaker() {
+                    BoneBreaker = 1; // Balgolgi
+                }
+                if willMakeSaltedFishCharger() {
+                    SaltedFishCharger = 1; // JeotgalChungjun-gi
+                }
+                if willMakeStarchSynthesizer() {
+                    StarchSynthesizer = 1; // JeonbunHapseong-gi
+                }
+                if willMakeFryer() {
+                    Fryer = 1; // Tuigimgi
+                }
                 break;
             case 5:
-                NormalSale = 1;
-                Freezing = 1;
-                Drying = 1;
-                Pickle = 1;
-                FishCake = 1;
-                Can = 1; // Tongjorim
+                if willMakeFreezer() {
+                    Freezer = 1; // Naengdong-gi
+                }
+                if willMakePackagingMachine() {
+                    PackagingMachine = 1; // Pojang-gi
+                }
+                if willMakeDryer() {
+                    Dryer = 1; // Geonjogi
+                }
+                if willMakeBoneBreaker() {
+                    BoneBreaker = 1; // Balgolgi
+                }
+                if willMakeSaltedFishCharger() {
+                    SaltedFishCharger = 1; // JeotgalChungjun-gi
+                }
+                if willMakeStarchSynthesizer() {
+                    StarchSynthesizer = 1; // JeonbunHapseong-gi
+                }
+                if willMakeFryer() {
+                    Fryer = 1; // Tuigimgi
+                }
+                if willMakeCanCharger() {
+                    CanCharger = 1; // TongjorimChungjeon-gi
+                }
+                if willMakeCanMaker() {
+                    CanMaker = 1; // TongjorimJejak-gi
+                }
                 break;
         }
     }
+
+    class ProcessMachine
+    {
+        var GeneralSale = 1; // General sale mean
+        var Freezing, Drying, SaltedFish, FishCake, Can: bool
+
+        if Freezer == 1 && PackagingMachine == 1: Freezing = 1; // Naengdong active
+        if Dryer == 1 && PackagingMachine == 1: Drying = 1; // Geonjo active
+        if BoneBreaker == 1 && SaltedFishCharger == 1 && PackagingMachine == 1: SaltedFish = 1; // Jeotgal active
+        if BoneBreaker == 1 && StarchSynthesizer == 1 && Fryer == 1 && PackagingMachine == 1: FishCake = 1; // Eomuk active
+        if BoneBreaker == 1 && CanCharger == 1 && CanMaker == 1 && PackagingMachine == 1: Can = 1;
+    }
+        
+
 }
