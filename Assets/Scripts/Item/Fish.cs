09936cffa1aefad1dd6630201aa96947d25b6acd
G-POS <hwangck0827@gmail.com>
2024-08-14 03:58:38 +0900

    [Feat] Fish Class Fixed

---
+++ Assets/Scripts/Item/Fish.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public class Fish 
+{
+    public int itemID;
+    public string fishName;
+    public bool waterType;
+    public int fishTear;
+    public int growTime;
+    public double feedAmount;
+    public int sellPrice;
+    public int buyPrice;
+}
