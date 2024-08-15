cd40b2db075a756c046007f38867622f0830ac19
안현태 <ahnpostech@postech.ac.kr>
2024-08-15 21:43:34 +0900

    Update Recipe.cs

--- Assets/Scripts/ItemClass/Recipe.cs
+++ Assets/Scripts/ItemClass/Recipe.cs
@@ -1,11 +1,18 @@
 using System;
 
 [Serializable]
+public class ItemHandler
+{
+    public int ID;
+    public int num;
+}
+[Serializable]
 public class Recipe
 {
     public int recipeID;
     public string recipeName;
-    public int[] ingredientID;
+    public ItemHandler[] ingredient;
     public int[] machineID;
     int dayRequired;
+    public ItemHandler result;
 }
