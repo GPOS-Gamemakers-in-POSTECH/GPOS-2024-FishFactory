10d490599e5ff9dad9fd969f0ef7992a24ea1d7b
G-POS <hwangck0827@gmail.com>
2024-08-16 16:47:27 +0900

    [Feat] Completed Item Data

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System;
 
 [Serializable]
@@ -12,6 +13,14 @@ public class Item
     public int sellPrice;
     public int buyPrice;
     public int power;
+
+    public void ShowItemInfo()
+    {
+        Debug.Log(itemID);
+    }
+
+    public void GeneratePower(int increasePower) { power += increasePower; } // Increase Power Value
+    public void DecreasePower(int decreasePower) { power -= decreasePower; } // Decrease Power Value
 }
 
 [Serializable]
--- Assets/Scripts/TestSceneScript.cs
+++ Assets/Scripts/TestSceneScript.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class TestSceneScript : MonoBehaviour
 {
     JsonManager jsonManager;
-    Dictionary<int, Item> itemDict;
+    Dictionary<int, Item>[] itemDict;
 
     // Start is called before the first frame update
     void Start()
     {
-        itemDict = new Dictionary<int, Item>();
-        jsonManager=FindObjectOfType<JsonManager>();
+        itemDict = new Dictionary<int, Item>[3];
+        itemDict[0] = new Dictionary<int, Item>();
+        itemDict[1] = new Dictionary<int, Item>();
+        itemDict[2] = new Dictionary<int, Item>();
+
+        jsonManager =FindObjectOfType<JsonManager>();
         jsonManager.LoadJson(itemDict);
 
-        Debug.Log(itemDict[10100].itemName);
+        Debug.Log(itemDict[0][10100].itemName);
     }
 }
