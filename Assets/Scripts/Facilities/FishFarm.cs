a82cbcd3b697fa9d0134445dabaae3d606b24fc0
G-POS <hwangck0827@gmail.com>
2024-08-19 07:58:16 +0900

    [Feat] Implemented Basic Fish Farm Class

--- Assets/Scripts/Facilities/FishFarm.cs
+++ Assets/Scripts/Facilities/FishFarm.cs
@@ -6,35 +6,87 @@ public class FishFarm
 {
     public int farmTear;
     public Item fish;
-    public int[] fishAmount;
-    public bool[] isFacilityOn;
+    public int fishAmount;
+    public Item[] parts;
+    public bool[] isPartsOn;
     public int dieCount;
     public int growCount;
 
-    public FishFarm(int farmTear, Item fish, int[] fishAmount, bool[] isFacilityOn)
+    // Constructor
+    public FishFarm(int farmTear, Item fish, Item[] parts, int fishAmount)
     {
         this.farmTear = farmTear;
         this.fish = fish;
+        this.parts = parts;
         this.fishAmount = fishAmount;
-        this.isFacilityOn = isFacilityOn;
+        isPartsOn = new bool[]{false, false, false, false};
         dieCount = 0;
         growCount = 0;
     }
 
+    // Show the Information of fish farm
     public void ShowFishFarmInfo()
     {
         Debug.Log(farmTear);
         Debug.Log(fish.itemID);
         Debug.Log(fish.itemName);
         Debug.Log(fishAmount);
-        Debug.Log(isFacilityOn);
+        Debug.Log(isPartsOn);
     }
 
-    public void CalculateCount()
+    // Add New Parts to fish farm
+    public void AddParts(Item newParts)
     {
-        if (isFacilityOn[0] == false && isFacilityOn[1] == false) { dieCount += 1; }
-        else { growCount += 1; }
+        for (int i = 0; i < 4; i++)
+        {
+            if (parts[i].itemID / 1000 == newParts.itemID / 1000)
+            {
+                if (parts[i].itemID == newParts.itemID) { Debug.Log("이미 장착된 파츠입니다."); }
+                else if (parts[i].itemID < newParts.itemID)
+                {
+                    parts[i] = newParts;
+                    Debug.Log("파츠 교체 완료");
+                }
+                else { Debug.Log("하위 티어의 파츠입니다."); }
+
+                return;
+                    
+            }
+            else { Debug.Log("장착할 수 없는 아이템입니다."); }
+        }
+
+        return;
+    }
+
+    // Check Parts of fish farm
+    public void CheckPartsOn()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (parts[i].itemID != 0) { isPartsOn[i] = true; }
+            else { isPartsOn[i] = false; }
+        }
+
+        return;
+    }
+
+    // Feed Fishes
+    public void FeedFish()
+    {
+        if (isPartsOn[3] == true)
+        {
+            parts[3].feedAmount -= fish.feedAmount * fishAmount;
+        }
+    }
+
+    // Check fish's grown days or days to die
+    public void DecideFishHealth()
+    {
+        if (isPartsOn[0] == false && isPartsOn[1] == false) { dieCount += 1; }
+        else { dieCount = 0; growCount += 1; }
 
         if (dieCount == 3) { }
+
+        return;
     }
 }
