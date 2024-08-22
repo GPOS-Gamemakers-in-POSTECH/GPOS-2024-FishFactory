e8ca33b30e1e4864a8de500174829804c2111c84
안현태 <ahnpostech@postech.ac.kr>
2024-08-22 15:36:02 +0900

    Update ShowItemInfo Debug
    better to show

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Diagnostics;
 
 [Serializable]
 public class Item
@@ -8,7 +9,7 @@ public class Item
     public string itemName;
     public bool waterType;
     public int itemTier;
-    public int growTime;
+    public int time;
     public double feedAmount;
     public int sellPrice;
     public int buyPrice;
@@ -16,13 +17,23 @@ public class Item
 
     public void ShowItemInfo()
     {
-        Debug.Log(itemID);
+        string debugLine = itemID.ToString()+ " ; " + itemName + " : /t";
+        int itemType = itemID / 10000;
+        if (itemType == 1 || itemType == 2 || itemType == 4)
+            debugLine += "ItemTier:" + itemTier.ToString();
+        if ((itemType == 1 && itemID % 10 == 0) || (itemType == 2 && itemID % 10 == 0) || itemType == 9)
+            debugLine += " | " + time.ToString();
+        if (itemType >= 1 && itemType <= 2)
+            debugLine += ", " +(!waterType?"Freshwater":"Ocean")+", FeedAmount: " + feedAmount.ToString();
+        if (itemType >= 1 && itemType <= 4)
+            debugLine += " | " + sellPrice.ToString() + "/" + buyPrice.ToString();
+        if (itemType == 9)
+            debugLine += " Power:" + power.ToString();
+        UnityEngine.Debug.Log(debugLine);
     }
 
-    // Increase Power Value
-    public void GeneratePower(int increasePower) {
-        power += increasePower;
-    }
+
+    public void GeneratePower(int increasePower) { power += increasePower; } // Increase Power Value
 
     public void DecreasePower(int decreasePower) { power -= decreasePower; } // Decrease Power Value
 }
