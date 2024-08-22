8bfe078b3899b0ab4330a08aec2f51bbb7232fe9
G-POS <hwangck0827@gmail.com>
2024-08-22 20:35:08 +0900

    [Design] Changed Item Sprites Name

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,4 +21,17 @@ public class GameManager : MonoBehaviour
         }
         else { Destroy(gameObject); }
     }
+
+    // Load Item Data from Json
+    public void LoadJson(Dictionary<int, Item>[] itemDict)
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>("Json/Item");
+        ItemData itemData = JsonUtility.FromJson<ItemData>(textAsset.text);
+
+        foreach (Item item in itemData.fishes) { itemDict[0].Add(item.itemID, item); }
+        foreach (Item item in itemData.tools) { itemDict[1].Add(item.itemID, item); }
+        foreach (Item item in itemData.facilities) { itemDict[2].Add(item.itemID, item); }
+
+        return;
+    }
 }
